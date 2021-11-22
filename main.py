abcb22b02797987050c109ad6d7232901071aa2f
Shubham Gantayat <sgantayat9@gmail.com>
2021-11-23 03:21:33 +0530

    implementing ci/cd

---
+++ main.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from wsgiref import simple_server
+import os
+import flask_monitoringdashboard as dashboard
+from flask_cors import CORS, cross_origin
+
+app = Flask(__name__)
+dashboard.bind(app)
+CORS(app)
+
+
+@app.route('/', methods=['GET'])
+@cross_origin()
+def home():
+    return "Flask app is running"
+
+
+if __name__ == '__main__':
+    host = '0.0.0.0'
+    port = int(os.getenv("PORT", 5000))
+    httpd = simple_server.make_server(host=host, port=port, app=app)
+    httpd.serve_forever()
+
+
