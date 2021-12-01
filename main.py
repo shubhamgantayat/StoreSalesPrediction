0389a9d620162fab3c158873c43a5cf2face8bbd
Shubham Gantayat <sgantayat9@gmail.com>
2021-12-01 18:17:41 +0530

    deploy debug 7

--- main.py
+++ main.py
@@ -131,8 +131,8 @@ def predict_data():
             config.logger.log("INFO", os.getcwd())
             config.logger.log("INFO", os.listdir())
             folder_path = 'prediction_raw_files'
-            filepath = folder_path + '/' + 'prediction_raw_data_' + str(datetime.datetime.now().strftime("%Y%m%d%H%M%S")) + ".csv"
-            # filepath = os.path.join(folder_path, filename)
+            filename = 'prediction_raw_data_' + str(datetime.datetime.now().strftime("%Y%m%d%H%M%S")) + ".csv"
+            filepath = folder_path + '/' + filename
             test_data = pd.DataFrame.from_dict(data)
             # test_data.to_csv(filename, index=False)
             config.logger.log("INFO", "File saved")
